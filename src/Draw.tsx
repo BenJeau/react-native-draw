b35d0fae67700031ce3573c9ed7fc5453496b803
Benoit Jeaurond <benoit@jeaurond.dev>
2020-12-24 10:43:00 -0500

    fix: remove built-in styling

--- src/Draw.tsx
+++ src/Draw.tsx
@@ -561,7 +561,6 @@ const Draw = forwardRef<DrawRef, DrawProps>(
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
     justifyContent: 'center',
     alignItems: 'center',
   },
